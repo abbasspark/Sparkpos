7712691d3a22cb0a81e20445e02ef8f6c4ddc0e2
abbasspark <abbasbandar@gamil.com>
2021-05-12 17:50:17 +0200

    #body

--- src/SiteData.js
+++ src/SiteData.js
@@ -195,10 +195,10 @@ export const footer = {
     lable: "SITE MAP",
     items: [
       { id: 1, name: "Home", link: "/#body" },
-      { id: 2, name: "Services", link: "/services" },
-      { id: 3, name: "Download", link: "/download" },
-      { id: 4, name: "Contact", link: "/contact" },
-      { id: 5, name: "About", link: "/about" },
+      { id: 2, name: "Services", link: "/services#body" },
+      { id: 3, name: "Download", link: "/download#body" },
+      { id: 4, name: "Contact", link: "/contact#body" },
+      { id: 5, name: "About", link: "/about#body" },
     ],
   },
   software: {
